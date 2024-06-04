54adf4313ad2cda942c7926f62806c26f6049295
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2024-06-04 09:43:16 +0700

    Add services folder

---
+++ src/services/DrinkService.js
@@ -0,0 +1,21 @@
+import axios from "./customized-axios";
+
+export const postCreateDrink = (data) => {
+  return axios.post("/api/drinks", data);
+};
+
+export const fetchGetAllDrinks = () => {
+  return axios.get("/api/drinks");
+};
+
+export const fetchGetDrinkById = (id) => {
+  return axios.get(`/api/drinks/${id}`);
+};
+
+export const putUpdateDrink = (id, data) => {
+  return axios.put(`/api/drinks/${id}`, data);
+};
+
+export const deleteDrinkById = (id) => {
+  return axios.delete(`/api/drinks/delete/${id}`);
+};
