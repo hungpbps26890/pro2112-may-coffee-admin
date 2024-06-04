f89be07958b7858518fa2e666877b72baea639c9
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2024-06-04 09:36:01 +0700

    Add templatemo admin asset

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import "./assets/css/font-awesome.min.css";
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/templatemo-style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
